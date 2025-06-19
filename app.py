2b931af49134f2268178ca2664cafcae57080f43
anna.salmela <anna.salmela@adalyon.com>
2025-06-19 13:19:56 +0300

    add empty message as placeholder

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ from rag_engine import load_index_and_chunks, get_answer
 
 st.set_page_config(page_title="Chat with my thesis", layout="wide")
 
+
 # Load RAG components
 @st.cache_resource
 def init_rag():
     return load_index_and_chunks()
 
+
 chunks, index, model = init_rag()
 
 st.title("Chat with my thesis")
-st.markdown("This app uses Mistral free tier LLM with rate limitations, which might make the app unavailable at times. Thank you for your patience!\n\nVoit kokeilla myös suomeksi.")
+st.markdown(
+    "This app uses Mistral free tier LLM with rate limitations, which might make the app unavailable at times. Thank you for your patience!\n\nVoit kokeilla myös suomeksi."
+)
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -24,15 +28,20 @@ for message in st.session_state.messages:
 # User input (automatically placed at bottom)
 if prompt := st.chat_input("Ask something about the thesis..."):
     # Display user message
-    st.session_state.messages.append({"role": "user", "content": prompt, "avatar": "❓"})
+    st.session_state.messages.append(
+        {"role": "user", "content": prompt, "avatar": "❓"}
+    )
     with st.chat_message("user", avatar="❓"):
         st.markdown(prompt)
 
     # Get assistant response
     with st.chat_message("assistant", avatar="👩‍🎓"):
-        with st.spinner("Thinking..."):
+        message_placeholder = st.empty()
+        with st.spinner("..."):
             response = get_answer(prompt, index, chunks, model)
-            st.markdown(response)
+            message_placeholder.markdown(response)
 
     # Save assistant message
-    st.session_state.messages.append({"role": "assistant", "content": response, "avatar": "👩‍🎓"})
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response, "avatar": "👩‍🎓"}
+    )
--- rag_engine.py
+++ rag_engine.py
@@ -1,17 +1,14 @@
 import pickle, faiss
 from sentence_transformers import SentenceTransformer
 from mistralai import Mistral
-import requests
-from requests.exceptions import HTTPError, ConnectionError, Timeout
-import numpy as np
 import os
 import time
-from getpass import getpass
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key= os.getenv("MISTRAL_API_KEY")
+api_key = os.getenv("MISTRAL_API_KEY")
 client = Mistral(api_key=api_key)
+
 
 def load_index_and_chunks():
     with open("vector_store/doc_chunks.pkl", "rb") as f:
@@ -20,31 +17,17 @@ def load_index_and_chunks():
     model = SentenceTransformer("intfloat/multilingual-e5-small")
     return chunks, index, model
 
+
 def run_mistral(user_message, model="mistral-large-latest"):
-    messages = [
-        {
-            "role": "user", "content": user_message
-        }
-    ]
-    
+    messages = [{"role": "user", "content": user_message}]
+
     for attempt in range(3):
         try:
-            chat_response = client.chat.complete(
-                model=model,
-                messages=messages
-            )
-            return (chat_response.choices[0].message.content)
-        except HTTPError as e:
-            if e.response.status_code == 429:
-                print("⚠️ Rate limit hit. Retrying...")
-                time.sleep(2 ** attempt)  # exponential backoff
-            else:
-                raise e  # re-raise unexpected HTTP errors
-        except (ConnectionError, Timeout) as e:
-            print(f"🔌 Network error: {e}. Retrying...")
-            time.sleep(2 ** attempt)     
+            chat_response = client.chat.complete(model=model, messages=messages)
+            return chat_response.choices[0].message.content
         except Exception as e:
-            print(f"❌ Unexpected error: {e}")
+            print(f"Unexpected error: {e}")
+            time.sleep(2**attempt)  # exponential backoff
             break
 
     return "Sorry, I couldn't get a response from Mistral. Please try again later."
@@ -53,7 +36,7 @@ def run_mistral(user_message, model="mistral-large-latest"):
 def get_answer(query, index, chunks, model):
     query_vector = model.encode([query])
     D, I = index.search(query_vector, k=10)
-    context = "\n".join([" ".join(chunks[i-5:i+5]) for i in I[0]])
+    context = "\n".join([" ".join(chunks[i - 5 : i + 5]) for i in I[0]])
     prompt = f"""
         Context information is below.
         ---------------------
